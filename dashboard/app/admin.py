01e13f4e25ade63712f92b618af4c50df04b0d23
Rahul Thorat <137529661+coderRahul2604@users.noreply.github.com>
2024-07-12 22:41:43 +0530

    Delete dashboard directory

--- dashboard/app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from app.models import Insight
-
-# Register your models here.
-
-@admin.register(Insight)
-class InsightAdmin(admin.ModelAdmin):
-    list_display=['intensity','sector','topic']
-    search_fields = ['topic']
