caf2953cc4256943f2bf784bc1c7ec5b9f2f3d81
Rahul Thorat <137529661+coderRahul2604@users.noreply.github.com>
2026-05-05 02:13:15 +0530

    Allow all hosts in production settings

--- Numerology/settings.py
+++ Numerology/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*ai5cv7^q47(e*)-lrbx5bfvci$(ets&^q=!+_ag-#tgeq6@w1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
